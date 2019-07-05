30948f4942e9c439e5b27c69d0fefd4470365aa2
Lachlan Blennerhassett <lachlanblennerhassett@gmail.com>
2019-07-05 17:55:06 +1000

    TowerScript: track enemies using lists

--- TD Game/Assets/Scripts/SpawnerScript.cs
+++ TD Game/Assets/Scripts/SpawnerScript.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class SpawnerScript : MonoBehaviour {
 
 	public GameObject enemy;
-	public float timeBetweenMobs = 1.0f;
+	public float timeBetweenMobs = 2.0f;
 	public int enemiesRemainingToSpawn;
+    // track enemy transforms when spawned for use in TowerScript
+    public List<Transform> enemyTransforms = new List<Transform>();
 
 	// Use this for initialization
 	void Start () {
@@ -20,8 +22,13 @@ public class SpawnerScript : MonoBehaviour {
 		if(timeBetweenMobs <= 0.0f && enemiesRemainingToSpawn > 0) 
 		{
 			Instantiate(enemy);
+            enemyTransforms.Add(enemy.transform);
 			timeBetweenMobs = 2.0f;
 			enemiesRemainingToSpawn--;
 		}
+        foreach (Transform enemyTransform in enemyTransforms)
+        {
+            print(enemyTransform);
+        }
 	}
 }
--- TD Game/Assets/Scripts/TowerScript.cs
+++ TD Game/Assets/Scripts/TowerScript.cs
@@ -7,38 +7,74 @@ public class TowerScript : MonoBehaviour
 	public Material highlightTarget;
     // public float minRange = 5.0f;
     // try max range only
+    
     public float maxRange = 10.0f;
+
+    // create reference to SpawnerScript for later use
+    private SpawnerScript spawnScript;
+    // create empty list of enemy proximites to populate later
+    List<float> enemyProximities = null;
+
+
+    Vector3 towerPosition;
+    public void Start()
+    {
+        // create instance of SpawnerScript
+        spawnScript = new SpawnerScript();
+        Vector3 towerOffset = new Vector3(0, 1.5f, 0);
+        Vector3 towerPosition = transform.position + towerOffset;
+        print("Position of Tower is: " + towerPosition);
+        
+    }
+
+    
+    public void TrackEnemyProximities()
+    {
+
+        List<Transform> towerEnemyTransforms = spawnScript.enemyTransforms;
+        foreach (Transform towerEnemyTransform in towerEnemyTransforms)
+        {
+            float enemyProximity = towerEnemyTransform.position.magnitude - towerPosition.magnitude;
+            enemyProximities.Add(enemyProximity);
+        }
+    }
 
     public GameObject FindClosestEnemy(float max)
     {
-        GameObject[] enemies = GameObject.FindGameObjectsWithTag("enemy");
+        // THIS FUNCTION is going to execute every frame
+               
+        // GameObject[] enemies = GameObject.FindGameObjectsWithTag("enemy");
         GameObject closestEnemy = null;
-        Vector3 towerOffset = new Vector3(0, 1.5f, 0);
-        Vector3 towerPosition = this.transform.position + towerOffset;
-        print("Position of Tower is: " + towerPosition);
+        
+        
+        
 
         // reset closestDistance
         float closestDistance = Mathf.Infinity;
         
-        // track enemy transforms
-        List<Transform> enemyTransforms = new List<Transform>();
-        // track distances of each enemy
-        foreach (GameObject enemy in enemies)
-        {
-            enemyTransforms.Add(enemy.transform);
-        }
-        // print(enemyTransforms);
-        foreach (var enemyItem in enemyTransforms)
-        {
-            print("Enemy list transform is located at: " + enemyItem.transform.position);
-        }
+        
+        
+        //// track enemy transforms
+        //List<Transform> enemyTransforms = new List<Transform>();
+        //// track distances of each enemy
+        //foreach (GameObject enemy in enemies)
+        //{
+        //    enemyTransforms.Add(enemy.transform);
+        //}
+        //// print(enemyTransforms);
+        //foreach (var enemyItem in enemyTransforms)
+        //{
+        //    print("Enemy list transform is located at: " + enemyItem.transform.position);
+        //}
         // TO DO: track each enemyItem's transform and use for comparison to tower position
+
+
         
         // loop through all enemies in scene
         foreach (GameObject enemy in enemies)
         {
-            // reset object colour
-            enemy.GetComponent<Renderer>().material.color = Color.cyan;
+                //// reset object colour
+                //enemy.GetComponent<Renderer>().material.color = Color.cyan;
             // calculate distance between enemy and tower
             Vector3 diff = enemy.transform.position - towerPosition;
             // convert Vector3 to float with .magnitude
@@ -68,8 +104,17 @@ public class TowerScript : MonoBehaviour
         return closestEnemy;
     }
 
+    // updates every frame
     private void Update()
     {
+        
+        // create local list as copy of list in spawnScript instance
+        
         FindClosestEnemy(maxRange);
+    }
+
+    private void FixedUpdate()
+    {
+        
     }
 }
